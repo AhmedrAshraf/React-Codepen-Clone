bb567d280c102c98bcd73a6f3ce58a41e6e12630
Junaid Ahmed <junaidahmed31254@gmail.com>
2025-05-09 12:53:09 +0500

    Console feature added

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./styles.css";
 import Editor from "./Editor";
 import useLocalStorage from "../hooks/useLocalStorage";
@@ -8,18 +8,58 @@ export default function App() {
   const [css, setCss] = useLocalStorage("css", "");
   const [js, setJs] = useLocalStorage("js", "");
   const [srcDoc, setSrcDoc] = useState("");
+  const [logs, setLogs] = useState([]);
+
+  const iframeRef = useRef(null);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      setSrcDoc(`<html>
-			<body>${html}</body>
-			<style>${css}</style>
-			<script>${js}</script>
-			</html>`);
+      const injectedConsoleScript = `
+        (function() {
+          const log = console.log;
+          const error = console.error;
+          const warn = console.warn;
+
+          console.log = function(...args) {
+            parent.postMessage({ type: 'log', data: args }, '*');
+            log.apply(console, args);
+          };
+
+          console.error = function(...args) {
+            parent.postMessage({ type: 'error', data: args }, '*');
+            error.apply(console, args);
+          };
+
+          console.warn = function(...args) {
+            parent.postMessage({ type: 'warn', data: args }, '*');
+            warn.apply(console, args);
+          };
+        })();
+      `;
+
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${injectedConsoleScript + js}<\/script>
+        </html>
+      `);
     }, 250);
 
     return () => clearTimeout(timeout);
   }, [html, css, js]);
+
+  useEffect(() => {
+    const handleMessage = (event) => {
+      const { type, data } = event.data;
+      if (['log', 'error', 'warn'].includes(type)) {
+        setLogs(prev => [...prev, { type, data }]);
+      }
+    };
+
+    window.addEventListener("message", handleMessage);
+    return () => window.removeEventListener("message", handleMessage);
+  }, []);
 
   return (
     <>
@@ -43,8 +83,10 @@ export default function App() {
           onChange={setJs}
         />
       </div>
+
       <div className="pane">
         <iframe
+          ref={iframeRef}
           srcDoc={srcDoc}
           title="output"
           sandbox="allow-scripts"
@@ -52,6 +94,14 @@ export default function App() {
           width="100%"
           height="100%"
         />
+      </div>
+
+      <div className="console-pane">
+        {logs.map((log, index) => (
+          <div key={index} className={`log-${log.type}`}>
+            {log.data.join(" ")}
+          </div>
+        ))}
       </div>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import App from "./components/App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<App />,
   rootElement
 );
